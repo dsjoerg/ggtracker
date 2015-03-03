eb337b579da2a39af15599540b25bece9546ce83
David Joerg <dsjoerg@gmail.com>
2015-03-03 14:16:21 -0500

    keep IP cache in memory rather than on disk

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,6 +1,8 @@
 # In config/initializers/rack-attack.rb
 class Rack::Attack
 
+  Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+
   throttle('req/ip', :limit => 300, :period => 5.minutes) do |req|
     req.ip # unless req.path.starts_with?('/assets')
   end
