7706245151bd92954763723fc4425520adb1af11
dsjoerg <dsjoerg@gmail.com>
2016-04-13 09:04:45 -0400

    only do setdocdomain when we have an actual hostname, not just an IP address

--- app/assets/javascripts/setdocdomain.js
+++ app/assets/javascripts/setdocdomain.js
@@ -1,4 +1,8 @@
 // Proxino.log("gg.js domain was " + document.domain);
 // weird, was ggtracker.com even though script was served via CDN
-document.domain = /(\w+)(.\w+)?$/.exec(location.hostname)[0];
+if (location.hostname.match(/^[0-9.]*$/) == null) {
+    // only set document.domain if our location is a hostname like ggtracker.com or
+    // ggtracker.test.   But if it's an IP address, dont bother.
+    document.domain = /(\w+)(.\w+)?$/.exec(location.hostname)[0];
+}
 // Proxino.log("now gg.js domain is " + document.domain);
