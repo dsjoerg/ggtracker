5b2b1b6b12b527128beb06a446f76e4bd4be135a
dsjoerg <dsjoerg@gmail.com>
2017-07-29 12:36:33 -0400

    dont expose secret key for recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lcg3SkUAAAAAO4yYqDriPdh-er7islVavz1m-X2'
-  config.private_key = '6Lcg3SkUAAAAAEKwJT0Xy-U8XRC8_Qhc1w6FlZRv'
+  config.public_key  = '6Lc35yoUAAAAACUODLMbUxj4pC7NfUt5s-i76Z7D'
+  config.private_key = Rails.application.secret('recaptcha_secret')
 end
