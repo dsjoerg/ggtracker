bc6a2ce5bd7f4ddf38a0d4e404d4933243efbea2
dsjoerg <dsjoerg@gmail.com>
2018-01-15 19:56:47 -0500

    prevent people from signing up because were done here folks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
